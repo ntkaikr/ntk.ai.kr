cd5227d7b276748ec194a8eff950480f7059ed7e
ntk.ai.kr <ntk.ai.kr@gmail.com>
2025-04-18 13:19:41 +0900

    domain

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['43.203.89.143']
+ALLOWED_HOSTS = ['ntk.ai.kr', 'www.ntk.ai.kr', '43.203.89.143']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
