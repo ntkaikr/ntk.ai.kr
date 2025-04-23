79f48232586679964bb1b9e5fe2e6611bc15d989
ntk.ai.kr <ntk.ai.kr@gmail.com>
2025-04-23 22:20:59 +0900

    url changed card

--- carded/urls.py
+++ carded/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'carded'
 
 urlpatterns = [
-    path('<str:username>/', views.public_card_by_username, name='public_card_by_username'),  # ← 추가, 제일위로
     path('me/', views.my_card_view, name='my_card'),
-    path('', views.card_view, name='public_card'),  # ✅ 공개용 카드 뷰
     path('delete/<int:link_id>/', views.delete_social_link, name='delete_link'),
+    re_path(r'^(?P<username>(?!me$|delete$)[\w.@+-]+)/$', views.public_card_by_username, name='public_card_by_username'),
 ]
