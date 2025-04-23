118bcbcc0d11bad81112d2e161e08a4d03057008
ntk.ai.kr <ntk.ai.kr@gmail.com>
2025-04-23 21:43:19 +0900

    google favicon find

--- carded/utils.py
+++ carded/utils.py
@@ -1,8 +1,18 @@
+# carded/utils.py
+
 from urllib.parse import urlparse
 
+def extract_favicon_url(url):
+    parsed = urlparse(url)
+    domain = parsed.netloc
+    return f"https://www.google.com/s2/favicons?domain={domain}"
+
+"""
 def extract_favicon_url(site_url):
     try:
         parsed = urlparse(site_url)
         return f"{parsed.scheme}://{parsed.netloc}/favicon.ico"
     except:
         return ""
+
+"""
