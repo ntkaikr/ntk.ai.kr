294f4aeaac8f62bc4bc323d9f08dae829b0c3b5e
ntk.ai.kr <ntk.ai.kr@gmail.com>
2025-04-23 22:05:56 +0900

    copy aleart and correct path

--- carded/urls.py
+++ carded/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'carded'
 
 urlpatterns = [
+    path('<str:username>/', views.public_card_by_username, name='public_card_by_username'),  # ← 추가, 제일위로
     path('me/', views.my_card_view, name='my_card'),
     path('', views.card_view, name='public_card'),  # ✅ 공개용 카드 뷰
-    path('<str:username>/', views.public_card_by_username, name='public_card_by_username'),  # ← 추가
     path('delete/<int:link_id>/', views.delete_social_link, name='delete_link'),
 ]
