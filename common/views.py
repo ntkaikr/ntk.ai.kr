bb56a13accae1da8339c3cd859319efb1e2430c7
ntk.ai.kr <ntk.ai.kr@gmail.com>
2025-04-24 00:33:29 +0900

    profile model plan added'

--- common/views.py
+++ common/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 def page_not_found(request, exception):
     return render(request, 'common/404.html', {})
 
-def server_error(request, exception):
-    return render(request, 'common/500.html', status=500)
-    #return render(request, 'common/500.html', {})
+def server_error(request):
+    #return render(request, 'common/500.html', status=500)
+    return render(request, 'common/500.html', {})
--- myprofile/models.py
+++ myprofile/models.py
@@ -2,6 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 from toolhub.models import Tool  # 툴 모델 import
 
+# myprofile/models.py
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    plan = models.CharField(
+        max_length=10,
+        choices=[('free', '프리'), ('basic', '베이직'), ('premium', '프리미엄')],
+        default='free'
+    )
+    frequent_tools = models.ManyToManyField(Tool, blank=True, related_name='favored_by')
+
+    def tool_limit(self):
+        return {
+            'free': 3,
+            'basic': 10,
+            'premium': 30
+        }.get(self.plan, 3)
+
+    def __str__(self):
+        return f"{self.user.username}님의 프로필"
+"""
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     frequent_tools = models.ManyToManyField(Tool, blank=True, related_name='frequent_by')
@@ -17,6 +38,7 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}의 프로필"
+"""
 
 class Todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
