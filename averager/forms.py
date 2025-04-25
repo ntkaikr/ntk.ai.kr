3181d2aaf058fe9b4c57ee98ad8c4ae99fa0eed2
ntk.ai.kr <ntk.ai.kr@gmail.com>
2025-04-25 12:47:02 +0900

    better averager:

--- averager/forms.py
+++ averager/forms.py
@@ -1,7 +1,36 @@
+# averager/forms.py
 from django import forms
 
 class AveragingForm(forms.Form):
-    old_qty   = forms.DecimalField(label="기존 수량", min_value=0)
-    old_price = forms.DecimalField(label="기존 단가", min_value=0)
-    new_qty   = forms.DecimalField(label="추가 수량", min_value=0)
-    new_price = forms.DecimalField(label="추가 단가", min_value=0)
+    old_qty = forms.DecimalField(
+        label="기존 수량",
+        min_value=0,
+        widget=forms.NumberInput(attrs={
+            'class':'form-control',
+            'placeholder':'0'
+        })
+    )
+    old_price = forms.DecimalField(
+        label="기존 단가",
+        min_value=0,
+        widget=forms.NumberInput(attrs={
+            'class':'form-control',
+            'placeholder':'0'
+        })
+    )
+    new_qty = forms.DecimalField(
+        label="추가 수량",
+        min_value=0,
+        widget=forms.NumberInput(attrs={
+            'class':'form-control',
+            'placeholder':'0'
+        })
+    )
+    new_price = forms.DecimalField(
+        label="추가 단가",
+        min_value=0,
+        widget=forms.NumberInput(attrs={
+            'class':'form-control',
+            'placeholder':'0'
+        })
+    )
