d2c3290ee507c355f23916d866c7979b906a9e14
ntk.ai.kr <ntk.ai.kr@gmail.com>
2025-04-25 15:14:14 +0900

    lorem4

--- nlorem/views.py
+++ nlorem/views.py
@@ -1,20 +1,19 @@
+# nlorem/views.py
 from django.shortcuts import render
 from lorem_text import lorem
 from .forms import LoremForm
 
 def generate(request):
-    text = ''
-    paras = []               # 추가
+    paras = []
     if request.method == 'POST':
         form = LoremForm(request.POST)
         if form.is_valid():
             p = form.cleaned_data['paragraphs']
-            text = lorem.paragraphs(p)
-            paras = text.split('\n\n')   # 뷰에서 분할
+            # lorem.paragraph() 을 p번 호출해 명확히 문단 리스트 생성
+            paras = [lorem.paragraph() for _ in range(p)]
     else:
         form = LoremForm()
     return render(request, 'nlorem/generate.html', {
         'form': form,
-        'text': text,
-        'paras': paras,        # 컨텍스트에 넘겨줌
+        'paras': paras,
     })
