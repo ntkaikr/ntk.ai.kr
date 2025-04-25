91afa57851ec460c8d5389f9c316466b07257ba4
ntk.ai.kr <ntk.ai.kr@gmail.com>
2025-04-25 15:10:36 +0900

    ok lorem

--- nlorem/views.py
+++ nlorem/views.py
@@ -4,11 +4,17 @@ from .forms import LoremForm
 
 def generate(request):
     text = ''
+    paras = []               # 추가
     if request.method == 'POST':
         form = LoremForm(request.POST)
         if form.is_valid():
             p = form.cleaned_data['paragraphs']
             text = lorem.paragraphs(p)
+            paras = text.split('\n\n')   # 뷰에서 분할
     else:
         form = LoremForm()
-    return render(request, 'nlorem/generate.html', {'form': form, 'text': text})
+    return render(request, 'nlorem/generate.html', {
+        'form': form,
+        'text': text,
+        'paras': paras,        # 컨텍스트에 넘겨줌
+    })
