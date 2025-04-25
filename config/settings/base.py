712917e7fd4a8fb8b6cd1d26ea6ec50d737ae731
ntk.ai.kr <ntk.ai.kr@gmail.com>
2025-04-25 20:34:26 +0900

    nsys templates

--- config/settings/base.py
+++ config/settings/base.py
@@ -88,7 +88,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'nsys')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
